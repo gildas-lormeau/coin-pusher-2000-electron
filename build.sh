9f227084344cc51ac857e08ba66b766617d25bb8
Gildas <gildas.lormeau@gmail.com>
2025-07-13 16:45:26 +0200

    only include f64 build

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ rm -rf build
 npx vite build
 cp package.json build
 cp index.js build
-cp -R rapier3d-electron build/rapier3d-electron
 cp -R rapier3d-f64-electron build/rapier3d-f64-electron
 cp game/public/assets/* build/assets
 mkdir -p build/views
